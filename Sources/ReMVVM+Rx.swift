21b137f856b524986aa43e4b41e564cf52a7455a
Dariusz Grzeszczak <d.grzeszczak@gmail.com>
2021-08-25 07:46:13 +0200

    - initial commit

---
+++ Sources/ReMVVM+Rx.swift
@@ -0,0 +1,66 @@
+//
+//  ReMVVM+Rx.swift
+//  ReMVVMBasicExample
+//
+//  Created by Dariusz Grzeszczak on 11/01/2019.
+//  Copyright © 2019. All rights reserved.
+//
+
+import RxSwift
+import ReMVVMCore
+
+extension Store: ReactiveCompatible { }
+extension ReMVVM.State: ReactiveCompatible { }
+extension ReMVVM.Dispatcher: ReactiveCompatible { }
+
+extension Reactive: ObserverType where Base: Dispatcher {
+    public func on(_ event: Event<StoreAction>) {
+        guard let action = event.element else { return }
+        base.dispatch(action: action)
+    }
+}
+
+extension Reactive where Base: StateSource {
+
+    public var value: Observable<Base.State> {
+
+        return Observable.create { [base] observer in
+            let reactiveObserver = ReactiveObserver(observer)
+            base.add(observer: reactiveObserver)
+
+            return Disposables.create {
+                base.remove(observer: reactiveObserver)
+            }
+        }
+        .share(replay: 1)
+    }
+
+    private class ReactiveObserver: StateObserver {
+
+        let observer: AnyObserver<Base.State>
+        init(_ observer: AnyObserver<Base.State>) {
+            self.observer = observer
+        }
+
+        func didChange(state: Base.State, oldState: Base.State?) {
+            observer.onNext(state)
+        }
+    }
+}
+
+public protocol _Optional {
+    associatedtype Wrapped
+    var wrapped: Wrapped? { get }
+}
+
+extension Optional: _Optional {
+    public var wrapped: Wrapped? { self }
+}
+
+
+extension Reactive where Base: StateSource, Base.State: _Optional {
+
+    public var state: Observable<Base.State.Wrapped> {
+        return value.compactMap { $0.wrapped }
+    }
+}
